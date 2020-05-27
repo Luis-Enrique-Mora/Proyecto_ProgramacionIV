6d643206ffabab47e2b706719db8d8aa4cc167de
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-05-27 17:02:27 -0600

    controller userEmployee created index and get employee method, implemented

--- SamaraOrganicsSystem/Startup.cs
+++ SamaraOrganicsSystem/Startup.cs
@@ -29,6 +29,7 @@ namespace SamaraOrganicsSystem
         {
             services.AddDbContext<SamaraOrganicsServerContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,7 @@ namespace SamaraOrganicsSystem
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 

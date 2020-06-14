e900c8ca039659a7485450fdaa55f6faf474afb6
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-06-14 15:31:47 -0600

    MoneyAccountsController Created and index implemented

--- SamaraOrganicsSystem/Controllers/MoneyAccountsController.cs
+++ SamaraOrganicsSystem/Controllers/MoneyAccountsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SamaraOrganicsSystem.Data;
 
 namespace SamaraOrganicsSystem.Controllers
 {
@@ -11,5 +13,22 @@ namespace SamaraOrganicsSystem.Controllers
     [ApiController]
     public class MoneyAccountsController : ControllerBase
     {
+        private readonly SamaraOrganicsServerContext _db;
+        public MoneyAccountsController(SamaraOrganicsServerContext db)
+        {
+            _db = db;
+        }
+        [HttpGet("index")]
+        public async Task<IActionResult> Index()
+        {
+            var AccountsList = await _db.MoneyAccounts.Select(a => new { a.MoneyAccountId, a.NameMoneyAccount, a.DescriptionMoneyAccount }).ToListAsync();
+
+            if(AccountsList.Count > 0)
+            {
+                return Ok(AccountsList);
+            }
+
+            return NotFound("Sorry, We couldn't find any Account");
+        }
     }
 }
